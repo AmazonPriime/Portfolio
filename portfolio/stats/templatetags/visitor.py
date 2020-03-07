c7b97be95ec7e380765848c199f88676548f9bcd
AmazonPriime <lukeholland15500@gmail.com>
2020-03-07 10:48:48 +0000

    Created 2 stats template tags for updating site visitors and showing how many

---
+++ portfolio/stats/templatetags/visitor.py
@@ -0,0 +1,23 @@
+from django import template
+from stats.models import Visitor
+
+register = template.Library()
+
+@register.simple_tag
+def update_visitors(request):
+    if not request.session.session_key:
+        request.session.save()
+        key = request.session.session_key
+    else:
+        key = request.session.session_key
+
+    visitor = Visitor.objects.filter(visitor_id = key)
+    if not visitor:
+        visitor = Visitor(visitor_id = key)
+        visitor.save()
+
+    return
+
+@register.simple_tag
+def visitors_count():
+    return len(Visitor.objects.all())
