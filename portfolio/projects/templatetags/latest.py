9587bc63b196523dfa2cbc38430db9445013e72d
AmazonPriime <lukeholland15500@gmail.com>
2020-04-05 23:07:51 +0100

    Removed recently updated

--- portfolio/projects/templatetags/latest.py
+++ portfolio/projects/templatetags/latest.py
@@ -4,9 +4,5 @@ from projects.models import Project
 register = template.Library()
 
 @register.simple_tag
-def get_latest_update():
-    return Project.objects.all().order_by('-date_updated')[0]
-
-@register.simple_tag
 def get_newest_project():
     return Project.objects.all().order_by('-date_created')[0]
