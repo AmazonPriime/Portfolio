a03b9cb178e8d469726666d31691f0d1df3d4925
AmazonPriime <lukeholland15500@gmail.com>
2020-04-05 22:40:57 +0100

    Added recently updated and newest project as template tags

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -7,8 +7,8 @@ class Project(models.Model):
     language = models.CharField(max_length = 128)
     views = models.IntegerField(default = 0)
     url = models.URLField(default = "https://github.com")
-    date_created = models.DateField(default = datetime.now)
-    date_updated = models.DateField(default = datetime.now)
+    date_created = models.DateTimeField(default = datetime.now)
+    date_updated = models.DateTimeField(default = datetime.now)
 
     def __str__(self):
         return self.name
