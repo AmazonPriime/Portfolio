4d19f1f5abc43fbbafbf5baa8764cc24e9a2c49d
AmazonPriime <lukeholland15500@gmail.com>
2020-04-07 08:48:53 +0100

    Updated about information and tidied up the css file and have added the use of variables for the colours

--- portfolio/projects/templatetags/latest.py
+++ portfolio/projects/templatetags/latest.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.simple_tag
 def get_newest_project():
     return Project.objects.all().order_by('-date_created')[0]
+
+@register.simple_tag
+def get_updated_project():
+    return Project.objects.all().order_by('-date_updated')[0]
